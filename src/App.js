e91c5765eb48a6bc7d7586e9775ef1c351f0d7fc
Teo Bay Bong <phamanh9296@icloud.com>
2021-05-23 00:55:08 +0900

    change README.md

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const APP_ID= '8634c5e4';
+  const APP_KEY= '0390722093b24c9c8ad1ee0af10b32e4';
+  
   return (
     <div className="App">
       <h1>Hello React</h1>
