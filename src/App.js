61a2ce47cd514d82b39bc9fd76e5682b9f262752
Teo Bay Bong <phamanh9296@icloud.com>
2021-05-23 01:39:36 +0900

    add recipe component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
+import Recipe from './Components/Recipe/Recipe';
 
 function App() {
 
@@ -15,8 +16,8 @@ function App() {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
     const data = await response.json();
-    // console.log(data)
     setRecipes(data.hits);
+    console.log(data.hits);
   };
 
   return (
@@ -26,6 +27,14 @@ function App() {
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+          key= {recipe.recipe.label}
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image= {recipe.recipe.image}
+        /> 
+      ))}
     </div>
   );
 }
