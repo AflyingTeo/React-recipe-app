c4d559896aaf494309b4028bc80a0b98cac68f0d
Teo Bay Bong <phamanh9296@icloud.com>
2021-05-23 01:18:34 +0900

    fetch data in App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
 
   const APP_ID= '8634c5e4';
   const APP_KEY= '0390722093b24c9c8ad1ee0af10b32e4';
-  
+  const [recipes, setRecipes] = useState([]);
+  useEffect( () => {
+    // console.log('Effect has been run')
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    const data = await response.json();
+    // console.log(data)
+    setRecipes(data.hits);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
